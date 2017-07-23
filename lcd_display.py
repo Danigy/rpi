93713f0d3b2a32b2909cf3b106dcba3d5e5a53f3
bansalparijat <bansalparijat@gmail.com>
2017-07-23 23:21:56 +0530

    Changed display message

--- lcd_display.py
+++ lcd_display.py
@@ -50,14 +50,14 @@ E_DELAY = 0.0005
 # Main program block
 def main():
   
-	GPIO.setmode(GPIO.BOARD)       # Use BCM GPIO numbers
-	GPIO.setup(LCD_E, GPIO.OUT)  # E
-	GPIO.setup(LCD_RS, GPIO.OUT) # RS
-	GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  	GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  	GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  	GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-  	#GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable
+  GPIO.setmode(GPIO.BOARD)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+  GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable
  
   # Initialise display
   lcd_init()
@@ -73,24 +73,17 @@ def main():
   while True:
  
     # Send some centred test
-    lcd_string("--------------------",LCD_LINE_1,2)
-    lcd_string("Rasbperry Pi",LCD_LINE_2,2)
-    lcd_string("Model B",LCD_LINE_3,2)
+    lcd_string("Left Justified",LCD_LINE_1,1)
+    lcd_string("Center",LCD_LINE_2,2)
+    lcd_string("Right Justified",LCD_LINE_3,3)
     lcd_string("--------------------",LCD_LINE_4,2)
- 
-    time.sleep(3) # 3 second delay
- 
-    lcd_string("Raspberrypi-spy",LCD_LINE_1,3)
-    lcd_string(".co.uk",LCD_LINE_2,3)
-    lcd_string("",LCD_LINE_3,2)
-    lcd_string("20x4 LCD Module Test",LCD_LINE_4,2)
  
     time.sleep(3) # 20 second delay
  
     # Blank display
     lcd_byte(0x01, LCD_CMD)
  
-    time.sleep(3) # 3 second delay
+    time.sleep(1) # 3 second delay
  
 def lcd_init():
   # Initialise display
